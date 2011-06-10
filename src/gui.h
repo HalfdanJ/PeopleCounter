3c84fae994ffee6e04e6d892f68c68c97fbf06e6
Johan <jbl@1calendar.com>
2011-06-10 18:07:17 +0200

    Added hotkeys for gui.

--- src/gui.h
+++ src/gui.h
@@ -5,12 +5,13 @@
 
 
 
-
 class Gui {
 public:
 	void setup(Kinect* kinectRef, Tracker * trackerRef);
 	void update();
 	void draw();
+	
+	void keyPressed(int key);
 	
 	Kinect * kinect;
 	Tracker * tracker;
